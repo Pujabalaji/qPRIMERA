415fa756562f9ca5a86a6ca698c9f711bb15a277
Riley Kong <rileyis@nightingale.csail.mit.edu>
2023-05-09 18:47:27 -0400

    Fix LDA issues

--- utils/lda.py
+++ utils/lda.py
@@ -17,71 +17,80 @@ nltk.download('punkt')
 tokenizer = AutoTokenizer.from_pretrained('./PRIMER')
 print('tokenizer loaded')
 
-def lda_preprocess(batch_num, out_csv_path=None, alpha="symmetric", beta="auto", score_filt=0.9, min_docs=5, top_n=None):
+def lda_preprocess(batch_num, sent_doc=False, num_topics=20, alpha="asymmetric", beta="auto", score_filt=0.95, min_docs=3, top_n=None):
     """
     Uses LDA to select documents from a csv file.
 
     Arguments:
         * batch_num: the file path to the input batch csv
-        * out_csv_path: the file path to the output csv
-        * alpha (default 0.5): alpha parameter, a lower value indicates fewer topics per document
-        * beta (default 0.1): beta parameter, a lower value indicates fewer words per topic
-        * score_filt (default 0.5): filters for all documents with a similarity greater than it
-        * min_docs (default 5): must include at least 5.
+        * sent_doc: whether to treat each individual sentence as a distinct document
+        * alpha (default auto): alpha parameter, a lower value indicates fewer topics per document
+        * beta (default auto): beta parameter, a lower value indicates fewer words per topic
+        * score_filt (default 0.9): filters for all documents with a similarity greater than it
+        * min_docs (default 3): must include at least min_doc docs.
         * top_n (default None): if specified, selects the top n documents directly, 
                 ignoring score_filt
 
-    Returns:
+    Returns: the list of pruned documents.
 
     """
 
-    samples = read_data_from_csv(batch_num)
+    data = read_data_from_csv(batch_num)
     
     pruned_docs = []
-    for sample in tqdm(samples):
+    for datum in tqdm(data):
+        query, sample = datum["query"], datum["docs"]
+        
+        print(query)
         docs = []
-        for doc in sample:
-            for sent in tokenize.sent_tokenize(doc):
-                docs.append(sent)
+        
+        if sent_doc:
+            for doc in sample:
+                for sent in tokenize.sent_tokenize(doc):
+                    docs.append(sent)
+        else:
+            docs = sample
 
         tokens = [tokenizer(doc)['input_ids'] for doc in docs]
         query = tokenizer(query)['input_ids']
 
-        corpus = [list(Counter(doc).items()) for doc in tokens]
-        query_bow = list(Counter(query).items())
+        corpus = [list(Counter(doc).items()) for doc in (tokens + [query])]
+        # query_bow = list(Counter(query).items())
 
         # Train the model on the corpus.
-        lda = LdaModel(corpus, alpha, eta=beta) # for some reason gensim called it eta instead of beta
+        lda = LdaModel(corpus, num_topics=num_topics, alpha=alpha, eta=beta) # for some reason gensim called it eta instead of beta
 
 
         rows = []
         cols = []
         probs = []
 
-        for i, doc in enumerate(corpus+[query_bow]):
+        for i, doc in enumerate(corpus):
             for topic, prob in lda[doc]:
                 rows.append(i)
                 cols.append(topic)
                 probs.append(prob)
 
-        sparse = csr_matrix((probs, (rows, cols)), shape=(len(corpus)+1, 100))
+        sparse = csr_matrix((probs, (rows, cols)), shape=(len(corpus), 100))
         sims = cosine_similarity(sparse)[-1,:-1]
-        print(sims)
+        # print(sims)
 
         if top_n is not None:
-            top_n_doc_indices = np.argpartition(sims, -top_n)[-top_n:]
+            top_n_doc_indices = sorted(np.argpartition(sims, -top_n)[-top_n:])
             res = [docs[i] for i in top_n_doc_indices]
         else:
             res = [docs[i] for i in range(len(sims)) if sims[i] > score_filt]
 
         if len(res) < min_docs:
-            top_n_doc_indices = np.argpartition(sims, -min_docs)[-min_docs:]
+            actual_min = min(min_docs, len(docs))
+            top_n_doc_indices = sorted(np.argpartition(sims, -actual_min)[-actual_min:])
             res = [docs[i] for i in top_n_doc_indices]
 
-        pruned_docs.append("|||||".join(res))
+        # assert len(res) >= min(min_docs, len(docs))
+        pruned_docs.append(" ||||| ".join(res))
         print(pruned_docs[-1])
     
     return pruned_docs
 
 if __name__ == '__main__':
-    print(lda_preprocess(0))
+    lda_preprocess(0, sent_doc=True)
--- utils/read_batch.py
+++ utils/read_batch.py
@@ -1,4 +1,5 @@
 import csv
+import os
 
 def convert_str_to_list(string_list):
     string_list = string_list.strip("[]")
@@ -6,7 +7,7 @@ def convert_str_to_list(string_list):
     return [item[1:-1] for item in list_items]
 
 def read_data_from_csv(batch=0):
-    csv_file_path = f'../scraped_data/clean_text_from_urls_{batch}_final.csv'
+    csv_file_path = os.path.join(os.path.dirname(__file__), '..', 'scraped_data', f'clean_text_from_urls_{batch}_final.csv')
     data = []
     with open(csv_file_path, 'r', encoding='utf-8') as file:
         csv_reader = csv.reader(file)
